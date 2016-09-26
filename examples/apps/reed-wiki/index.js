d492ad7c72823a9b0e734437916f124f828ed4a5
David Reed <david.reed3@verizonwireless.com>
2016-09-26 16:05:54 -0400

    newapp files

---
+++ examples/apps/reed-wiki/index.js
@@ -0,0 +1,42 @@
+'use strict'
+module.change_code = 1;
+console.log('index.js');
+
+var alexa = require('alexa-app');
+
+// Define an alexa-app
+var app = new alexa.app('reed-wiki');
+//var HelperFunctions = require('./helper_functions');
+
+var WikiInfoIntent = require('./intents/wikiinfo_intent');
+var wikiinfoIntent = new WikiInfoIntent();
+
+//var AnswerIntent = require('./intents/answer_intent');
+//var answerIntent = new AnswerIntent();
+//var DontknowIntent = require('./intents/dontknow_intent');
+//var dontknowIntent = new DontknowIntent();
+//var StartOverIntent = require('./intents/startover_intent');
+//var startoverIntent = new StartOverIntent();
+
+
+//app.pre = require('./pre_event');
+
+app.launch(require('./launch_event'));
+
+app.intent('AnswerIntent', answerIntent.slots_and_utterances(), function (req, res) {answerIntent.callback(req, res)});
+app.intent('DontKnowIntent', dontknowIntent.slots_and_utterances(), function (req, res) {dontknowIntent.callback(req, res)});
+app.intent('StartOverIntent', startoverIntent.slots_and_utterances(), function (req, res) {startoverIntent.callback(req, res)});
+
+app.messages.NO_INTENT_FOUND = "Why did you call that intent? I don't know about that";
+
+app.sessionEnded(function (req, res) {
+    console.log('sessionEnded event');
+    // Clean up the user's server-side stuff, if necessary
+});
+
+app.error = function (exception, request, response) {
+    console.log(exception);
+    throw exception;
+};
+
+module.exports = app;
---
+++ examples/apps/reed-wiki/intents/wikiinfo_intent.js
@@ -0,0 +1,27 @@
+'use strict'
+module.change_code = 1;
+
+function Intent() {}
+
+Intent.prototype.slots_and_utterances = function () {
+
+    //console.log('slots_and_utterances');
+    var slots_and_utterances = {
+        'slots': {
+            'ANSWER': 'LIST_OF_ANSWERS'
+        },
+        "utterances": ["{|the answer is|my answer is|is it} {-|ANSWER}", "{-|ANSWER} is my answer"]
+    };
+    return slots_and_utterances;
+}
+
+// Callback for the Answer Intent
+Intent.prototype.callback = function (req, res) {
+    console.log('answerintent');
+    var Answer = require('./answer_process');
+    var answer = new Answer();
+
+    answer.process('answer', req, res);
+
+}
+module.exports = Intent
