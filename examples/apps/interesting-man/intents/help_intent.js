fe059ea6fe2762a356ca9b6d523d3e45d2b2c462
david <dreed10@gmail.com>
2016-10-09 21:19:21 -0400

    updates

--- examples/apps/interesting-man/intents/help_intent.js
+++ examples/apps/interesting-man/intents/help_intent.js
@@ -15,6 +15,7 @@ Intent.prototype.slots_and_utterances = function () {
 
 Intent.prototype.callback = function (req, res) {
     console.log('helpevent');
+    res.session('person', null);
     res.say("You can use this skill by saying something like. Tell me an interesting quote about Bill from Interesting man. Or, get us a manly fact about James from Intersting Man. If you would like to continue, say a name.").shouldEndSession(false, "If you would like to continue, say a name.");
     
     return;
