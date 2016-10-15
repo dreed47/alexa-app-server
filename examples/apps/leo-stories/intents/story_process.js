17ad9d765d7877b08521f16fd1ec3017301bd9c4
david reed <david@davids-Mac-mini.lan>
2016-10-15 09:26:46 -0400

    update

--- examples/apps/leo-stories/intents/story_process.js
+++ examples/apps/leo-stories/intents/story_process.js
@@ -10,8 +10,6 @@ Story.prototype.process = function (req, res) {
     this.inc = require('../inc/include');
     var stories = this.inc.stories;
 
-    //var story = req.session('story') ? (req.session('story') != 'nothing') : req.slot('STORY');
-    //var story_list = req.session('stories_list') ? (req.session('stories_list') != 'nothing') : stories;
     var story = req.slot('STORY');
     var story_list = stories;
  
@@ -19,17 +17,16 @@ Story.prototype.process = function (req, res) {
 
     var tellStory = story_list[story];
     
-    tellStory = _.replace(tellStory, /\|\|\|\|\|\|/g, "<break time='3000ms'/>")
-    tellStory = _.replace(tellStory, /\|\|\|\|\|/g, "<break time='2500ms'/>")
-    tellStory = _.replace(tellStory, /\|\|\|\|/g, "<break time='2000ms'/>")
-    tellStory = _.replace(tellStory, /\|\|\|/g, "<break time='1500ms'/>")
-    tellStory = _.replace(tellStory, /\|\|/g, "<break time='1000ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|\|\|\|\|\|/g, "<break time='3500ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|\|\|\|\|/g, "<break time='3000ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|\|\|\|/g, "<break time='2500ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|\|\|/g, "<break time='2000ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|\|/g, "<break time='1500ms'/>")
+//    tellStory = _.replace(tellStory, /\|\|/g, "<break time='1000ms'/>")
     tellStory = _.replace(tellStory, /\|/g, "<break time='500ms'/>")
 
     res.say(tellStory);
 
-    //res.session('story', story);
-    //res.session('stories_list', story_list);
     console.log("say = " + tellStory);
 
 }
